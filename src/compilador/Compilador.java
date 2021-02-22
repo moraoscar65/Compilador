e2d70f191d78d2f738384a46a90e0492db74d2e7
Oscar Mora <moraoscar65@gmail.com>
2021-02-22 14:15:45 -0600

    first commit

---
+++ src/compilador/Compilador.java
@@ -0,0 +1,21 @@
+
+package compilador;
+
+/**
+ *
+ * @author Jorge-PC
+ */
+public class Compilador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    Interfaz comp = new Interfaz();
+    comp.setVisible (true);
+
+    }
+    
+}
